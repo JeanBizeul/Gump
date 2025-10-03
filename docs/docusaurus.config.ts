ba76b02c1439cb9883e5d71f8c8fbabc448bc73e
Jean BIZEUL <146715844+JeanBizeul@users.noreply.github.com>
2025-10-03 12:33:31 +0200

    📝 Add basic docusaurus docs (#1)
    * 📝 Add basic docusaurus docs
    
    * 📝 Remove all useless stuff from the docusaurus docs & add the dev setup page
    
    * 📝🐛 use 'slug: /' in the header of the welcome page to make it the landing page of the website
    
    * 💚 Add a step to update all submodules in the C/C++ workflow
    
    * 👷 Make the deploy docusaurus workflow trigger only on changes to the /docs/ folder
    
    * 👷 Make the C-C++ workflow not trigger on changes on \docs\ folder
    
    * 📝 Add mention to the docusaurus website docs in the main README
    
    * 💚 Split the configure CMAKE step of C-C++ workflow
    
    * 💚 Empty commit to try to update the gh-pages branch & so the website
    
    * 💚 Empty commit to try to update the gh-pages branch & so the website
    
    * 👷 Add workflow to protect main branch from non-dev pull requests

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,110 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Gump',
+  favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://jeanbizeul.github.io',
+  baseUrl: '/Gump/',
+
+  // GitHub pages deployment config.
+  organizationName: 'JeanBizeul',
+  projectName: 'Gump',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: true,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          routeBasePath: '/', // Serve the docs at the site's root
+        },
+        blog: false,
+        theme: {
+          customCss: './custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    navbar: {
+      title: 'Gump Logo',
+      logo: {
+        alt: 'Gump Logo',
+        src: 'img/favicon.ico',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'gettingStartedSidebar',
+          position: 'left',
+          label: 'Getting Started',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'guidesSidebar',
+          position: 'left',
+          label: 'Guides',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Sections',
+          items: [
+            {
+              label: 'Getting Started',
+              to: '/',
+            },
+            {
+              label: 'Guides',
+              to: '/guides/setup',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/JeanBizeul/Gump',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © 2025 Grumpy, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
