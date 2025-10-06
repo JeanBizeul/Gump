c9d16c2b7abd38860329cf1c14efd99de3509189
Jean BIZEUL <146715844+JeanBizeul@users.noreply.github.com>
2025-10-06 02:48:34 +0200

    ✨ Create a window by using window class in main

--- app/Main.cpp
+++ app/Main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
+#include <conio.h>
 
 #include "Logger/Logger.hpp"
+#include "OpenGLUtils/Window.hpp"
 
 int main() {
     Logs::Logger::getInstance().setMinimumLogLevel(Logs::LogLevel::Debug);
 
-    LOG_DEBUG("Hello world !");
-    LOG_INFO("Hello world !");
-    LOG_WARNING("Hello world !");
-    LOG_ERROR("Hello world !");
-    LOG_FATAL("Hello world !");
+    try {
+        LOG_DEBUG("Creating the window ...");
+        OpenGLUtils::Window window(720, 480, "Hello window !");
+        LOG_DEBUG("done");
+    } catch (std::exception e) {
+        LOG_FATAL("Could not create the window: {}", e.what());
+        return 84;
+    }
     return 0;
 }
