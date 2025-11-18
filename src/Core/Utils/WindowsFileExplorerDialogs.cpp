c7498ad7f063c21593a266321decd970c1731650
Jean BIZEUL <146715844+JeanBizeul@users.noreply.github.com>
2025-11-18 05:25:15 +0100

    🚨Remove deprecated code in windows file picker tools

--- src/Core/Utils/WindowsFileExplorerDialogs.cpp
+++ src/Core/Utils/WindowsFileExplorerDialogs.cpp
@@ -6,17 +6,32 @@
 #include <codecvt>
 #include <locale>
 
+static std::wstring utf8ToWide(const std::string& str) {
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, nullptr, 0);
+    std::wstring wstr(size_needed, 0);
+    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, &wstr[0], size_needed);
+    wstr.resize(wcslen(wstr.c_str())); // trim null terminator
+    return wstr;
+}
+
+static std::string wideToUtf8(const std::wstring& wstr) {
+    int size_needed = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, nullptr, 0, nullptr, nullptr);
+    std::string str(size_needed, 0);
+    WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, &str[0], size_needed, nullptr, nullptr);
+    str.resize(strlen(str.c_str())); // trim null terminator
+    return str;
+}
+
+
 std::string Gump::Utils::openFilePickerDialog(const std::string& title, const std::string& filter)
 {
     OPENFILENAMEW ofn;
     std::wstring wFileName(MAX_PATH, L'\0');
-    std::wstring wFilter;
-    std::wstring wTitle;
 
     // Convert UTF-8 to wide strings for Win32 API
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> conv;
-    wFilter = conv.from_bytes(filter);
-    wTitle  = conv.from_bytes(title);
+    std::wstring wFilter = utf8ToWide(filter);
+    std::wstring wTitle  = utf8ToWide(title);
+
 
     // Replace '|' with '\0' for Win32 filter
     for (auto& ch : wFilter)
@@ -36,7 +51,7 @@ std::string Gump::Utils::openFilePickerDialog(const std::string& title, const st
     {
         wFileName.resize(wcslen(wFileName.c_str())); // trim extra nulls
         // Convert back to UTF-8 string
-        return conv.to_bytes(wFileName);
+        return wideToUtf8(wFileName);
     }
 
     return "";
