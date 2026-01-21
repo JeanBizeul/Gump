7926103fd97cc2f095df606133598590cee1599b
JeanBizeul <jean.bizeul@epitech.eu>
2026-01-21 11:03:28 +0100

    ✨ Add more settings to the layers

--- src/Core/UI/Layers.cpp
+++ src/Core/UI/Layers.cpp
@@ -1,7 +1,8 @@
 #include "UI/UI.hpp"
 
-#include <imgui/imgui.h>
-#include <imgui/imgui_internal.h>
+#include "imgui/imgui.h"
+#include "imgui/imgui_internal.h"
+#include "imgui/misc/cpp/imgui_stdlib.h"
 
 #include "Application.hpp"
 
@@ -9,6 +10,7 @@ void Gump::UI::renderLayers(Gump::Application &app) {
     // ImGuiWindowClass windowClass;
     // windowClass.DockNodeFlagsOverrideSet = ImGuiDockNodeFlags_AutoHideTabBar;
     // ImGui::SetNextWindowClass(&windowClass);
+    static bool popupOpen = false;
     ImGui::Begin("Layers");
 
     for (size_t i = 0; i < app.getLayerCount(); i++) {
@@ -44,6 +46,55 @@ void Gump::UI::renderLayers(Gump::Application &app) {
         // Layer name
         ImGui::SameLine();
         ImGui::Text("%s", layer->name.c_str());
+        if (ImGui::IsItemHovered())
+        {
+            ImGui::SetTooltip("Right click for Layer settings");
+        }
+        ImGui::OpenPopupOnItemClick("Layer settings", 1);
+
+        if (!popupOpen)
+            ImGui::SetNextWindowPos({ImGui::GetMousePos().x - 300, ImGui::GetMousePos().y - 75});
+
+        if (ImGui::BeginPopup("Layer settings")) {
+            popupOpen = true;
+
+            if (ImGui::InputText("Rename Layer", &layer->name, ImGuiInputTextFlags_EnterReturnsTrue)) {
+                popupOpen = false;
+                ImGui::CloseCurrentPopup();
+            }
+            ImGui::FocusItem();
+
+
+            ImGui::SameLine();
+            ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255,100,100,255));
+            if (ImGui::Button("Close") || Input::isKeyPressed(GLFW_KEY_ESCAPE)) {
+                ImGui::CloseCurrentPopup();
+                popupOpen = false;
+            }
+            ImGui::PopStyleColor();
+
+            ImGui::Separator();
+
+            if (ImGui::Button("Delete Layer")) {
+                app.getLayers().erase(app.getLayers().begin() + i);
+                popupOpen = false;
+                ImGui::EndPopup();
+                ImGui::End();
+                return; // Avoid going to bad layers ids
+            }
+
+            ImGui::SameLine();
+            if (ImGui::Button("Reset Layer Transformations")) {
+                layer->position = {0.0f, 0.0f};
+                layer->updateMesh();
+            }
+            if (ImGui::IsItemHovered())
+            {
+                ImGui::SetTooltip("Resets translations & rotations");
+            }
+
+            ImGui::EndPopup();
+        }
 
         // Visibility checkbox
         ImGui::Checkbox(("##visible" + std::to_string(i)).c_str(), &layer->isVisible);
